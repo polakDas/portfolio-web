bb853e09b688779645e312dfebbd964fdb293196
polakD <pulok3968@gmail.com>
2023-02-12 18:38:14 +0600

    bug fix -> app name

--- my_portfolio/my_diary/apps.py
+++ my_portfolio/my_diary/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class MyDiaryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "my_diary"
+    name = "my_portfolio.my_diary"
     verbose_name = "My Diary"
