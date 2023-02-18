402db375f49161d4f0bfc1ac0c213c9c2f519d79
polakD <pulok3968@gmail.com>
2023-02-19 02:24:03 +0600

    add diary routes

--- config/api_router.py
+++ config/api_router.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
+from my_portfolio.my_diary.api.views import DiaryViewSet
 from my_portfolio.users.api.views import UserViewSet
 
 if settings.DEBUG:
@@ -9,6 +10,7 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("diary", DiaryViewSet)
 
 
 app_name = "api"
